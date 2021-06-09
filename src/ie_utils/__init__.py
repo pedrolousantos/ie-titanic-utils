a6429f2aab30deae7b323d2dbc8fc28b5f937f81
Pedro Lourenço dos Santos <pedrosantos@student.ie.edu>
2021-06-09 23:56:38 +0200

    black

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -2,10 +2,12 @@
 IE Titanic utils.
 """
 
-__version__ = "0.1.0" # semver.org (semantic versioning)
+__version__ = "0.1.0"  # semver.org (semantic versioning)
+
 
 def tokenize(text):
     return text.split()
+
 
 if __name__ == "__main__":
     print(tokenize("Hello world"))
