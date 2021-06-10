d1394f3107afd0120e547abfa13d7c0ed101288c
Pedro Lourenço dos Santos <pedrosantos@student.ie.edu>
2021-06-11 00:28:56 +0200

    modifications forum

--- src/ie_utils/__main__.py
+++ src/ie_utils/__main__.py
@@ -2,9 +2,12 @@ from ie_utils import tokenize
 
 import sys
 
+#def main():
+ #   for word in sys.argv:
+  #      print(tokenize(word))
+
 def main():
-    for word in sys.argv:
-        print(tokenize(word))
+    print(tokenize(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
