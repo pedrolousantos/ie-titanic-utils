88efc7afd68ec06679aa89d261c548431ef4eb58
Pedro Lourenço dos Santos <pedrosantos@student.ie.edu>
2021-06-17 16:29:22 +0200

    new tests

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -4,11 +4,28 @@ IE Titanic utils.
 
 __version__ = "0.1.0"  # semver.org (semantic versioning)
 
+import re
+import pytest
 
-def tokenize(text, lower=False):
-    if lower:
-        text = text.lower()
-    return text.split()
+#def tokenize(text, lower=False):
+ #   if lower:
+  #      text = text.lower()
+   # return text.split()
+    
+def tokenize(text, lower=False, remove_punctuation=False, remove_stopwords=False):
+    if text == "":
+        raise ValueError("Cannot tokenize empty sentence")
+    else:
+        if lower:
+            text = text.lower()
+        if remove_punctuation:
+            text = re.sub("[.,!]", "", text)
+        if remove_stopwords:
+            stopwords = ["a", "the", "or", "and"]
+            words = text.split()
+            non_stop_words = [w for w in words if not w in stopwords]
+            text = " ".join(non_stop_words)    
+        return text.split()
 
 
 if __name__ == "__main__":
--- tests/test_ie_utils.py
+++ tests/test_ie_utils.py
@@ -19,3 +19,33 @@ def test_tokenize_lower_returns_lowercase_tokens():
     tokens = tokenize(sentence, lower=True)
     
     assert tokens == expected_tokens
+    
+def test_tokenize_remove_stopwords_returns__tokens():
+    sentence = "This is a sentence"
+    expected_tokens = ["This", "is", "sentence"]
+    
+    tokens = tokenize(sentence, remove_stopwords=True)
+
+    assert tokens == expected_tokens
+
+def test_tokenize_remove_punctuation_returns__tokens():
+    sentence = "This is a sentence."
+    expected_tokens = ["This", "is", "a", "sentence"]
+    tokens = tokenize(sentence, remove_punctuation=True)
+
+    assert tokens == expected_tokens
+
+def test_empty_string():
+    sentence = " "
+    tokens = tokenize(sentence)
+    with pytest.raises(ValueError) as exc_info:
+        if tokens == []:
+            raise ValueError("Cannot tokenize empty sentence")
+    assert exc_info.type is ValueError
+    
+def test_tokenize_parameters_returns__tokens():
+    sentence = "This is a sentence."
+    expected_tokens = ["this", "is", "sentence"]
+    tokens = tokenize(sentence, remove_punctuation=True, remove_stopwords = True, lower = True)
+
+    assert tokens == expected_tokens
